f8af2ecc294dc2811c1761c8420920409cd7afae
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-01-05 14:38:46 +0100

    badly abuse html+http to use a single form for all buttons of a device

--- infrared.py
+++ infrared.py
@@ -8,25 +8,27 @@ from bhbctrl import app, ctrl_funcs
 
 commands = {"asdf":"uiae"}
 devices = {
-    "Amplifier": {
-        "1":"asdf",
-        "2":"asdf",
-        "3":"asdf",
-        "4":"asdf",
-        "5":"asdf",
-        "6":"asdf"
-    },
-    "Projector":
-        {"abcd":"efgh"}
+    "Amplifier": [
+        ("1","asdf"),
+        ("2","asdf"),
+        ("3","asdf"),
+        ("4","asdf"),
+        ("5","asdf"),
+        ("6","asdf")
+    ],
+    "Projector":[
+        ("abcd","efgh")
+    ]
 }
 
 ctrl_funcs["send ir commands"] = "ir"
 @app.route('/ir', methods=['GET','POST'])
 def show_ir():
     if request.form:
-        cmd = request.form['cmd']
+        cmd = list(request.form)[0]
         send_cmd(cmd)
     return render_template("ir.html", devices = devices)
 
 def send_cmd(cmd):
     """ Send the specified infrared command. """
+    print("\033[32m>>> DEBUG\033[39m: command {} requested to be sent".format(cmd))
