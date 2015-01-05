9f2eaab55e347e841db7d0c03255eb94e0500bb2
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-01-05 11:51:15 +0100

    create actual infrared page, draft list of devices

--- infrared.py
+++ infrared.py
@@ -3,9 +3,30 @@ The infrared module allows control of an infrared transmitter at the Bytewerk,
 which in turn gives control over the amplifier, the video projector, etc…
 """
 
+from flask import render_template, request
 from bhbctrl import app, ctrl_funcs
 
+commands = {"asdf":"uiae"}
+devices = {
+    "Amplifier": {
+        "1":"asdf",
+        "2":"asdf",
+        "3":"asdf",
+        "4":"asdf",
+        "5":"asdf",
+        "6":"asdf"
+    },
+    "Projector":
+        {"abcd":"efgh"}
+}
+
 ctrl_funcs["send ir commands"] = "ir"
-@app.route('/ir')
+@app.route('/ir', methods=['GET','POST'])
 def show_ir():
-    return "hello IR"
+    if request.form:
+        cmd = request.form['cmd']
+        send_cmd(cmd)
+    return render_template("ir.html", devices = devices)
+
+def send_cmd(cmd):
+    """ Send the specified infrared command. """
