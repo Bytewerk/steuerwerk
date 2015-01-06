2fe5b758ecd66a3865dc5655583fb5d1c0c0dcdf
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-01-06 11:01:27 +0100

    implement primitive loading of IR codes from JSON
    A JSON file containing infrared control codes for appliances at the
    Bytewerk, which was previously used in another IR remote control
    project, can now be parsed and the IR control UI subsequently populated
    with the commands it defines.

--- infrared.py
+++ infrared.py
@@ -5,21 +5,25 @@ which in turn gives control over the amplifier, the video projector, etc…
 
 from flask import render_template, request
 from bhbctrl import app, ctrl_funcs
+import json
 
-commands = {"asdf":"uiae"}
-devices = {
-    "Amplifier": {
-        "1":"asdf",
-        "2":"asdf",
-        "3":"asdf",
-        "4":"asdf",
-        "5":"asdf",
-        "6":"asdf"
-    },
-    "Projector":{
-        "abcd":"efgh"
-    }
-}
+CMDS_FILE = "ircodes.json"
+devices = None
+with open(CMDS_FILE) as cmds_file:
+    devices = json.load(cmds_file)
+#devices = {
+#    "Amplifier": {
+#        "1":"asdf",
+#        "2":"asdf",
+#        "3":"asdf",
+#        "4":"asdf",
+#        "5":"asdf",
+#        "6":"asdf"
+#    },
+#    "Projector":{
+#        "abcd":"efgh"
+#    }
+#}
 
 ctrl_funcs["send ir commands"] = "ir"
 @app.route('/ir/', methods=['GET'])
@@ -27,7 +31,7 @@ ctrl_funcs["send ir commands"] = "ir"
 def show_ir(device=None):
     if request.form and device:
         name = request.form['cmd']
-        cmd = devices[device][name]
+        cmd = devices[device]["commands"][name]
         send_cmd(cmd)
     return render_template("ir.html", devices = devices)
 
