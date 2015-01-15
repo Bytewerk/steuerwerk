6a657aa86359545919ce436dc3f5170a3e4d17aa
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-01-15 22:15:07 +0100

    Throw in some JS for faster button interaction
    …at least in Browsers with JS.

---
+++ static/ir.js
@@ -0,0 +1,34 @@
+/*
+ * Small script to make XMLHttpRequests from the infrared control page
+ * so that buttons will respond more swiftly.
+ */
+"use strict";
+
+function sendCmd(target,cmd){
+	var req = new XMLHttpRequest();
+	var param = encodeURIComponent(cmd);
+	req.open("POST",target);
+	req.onreadystatechange = function() {
+		/* stop the web server from delivering the site content which the user can't see */
+		if (req.readyState == 2){ req.abort(); }
+	}
+	req.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // necessary to correctly submit the parameters
+	req.send("cmd="+cmd);
+}
+
+function initButtons(){
+	var forms = document.getElementsByTagName("form");
+	for(var f=0; f<forms.length; ++f){
+		var form = forms[f];
+		var buttons = form.getElementsByClassName("button");
+		for(var i=0; i<buttons.length; ++i){
+			var button = buttons[i];
+			button.onclick = function(){
+				sendCmd(form.action,button.value);
+				return false;
+			};
+		}
+	}
+}
+
+initButtons();
