6a08877f88f3c7e83bc4e901aad33adf35774a28
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-05-23 11:37:40 +0200

    CSS/HTML fixes, more Leuchtschild UI drafting

--- leuchtschild.py
+++ leuchtschild.py
@@ -5,12 +5,19 @@ The leuchtschild module allows control of the LED sign on the storefront.
 from steuerwerk import app, ctrl_funcs
 from flask import render_template
 
-functions = {
-	"select pattern": None,
-	"set color": None
+patterns = {
+	"eye cancer": None,
+	"red green blue": None,
+	"CSD": None,
+	"in Farbe und bunt": None
 }
 
 ctrl_funcs["control Leuchtschild"] = "leuchtschild"
-@app.route('/leuchtschild/')
+@app.route('/leuchtschild/',methods=["GET","POST"])
 def show_sign():
-	return render_template("leuchtschild.html",functions=functions)
+	return render_template("leuchtschild.html",patterns=patterns)
+
+## TODO ##
+# Indicate currently active pattern in web interface
+# Give previews/descriptions of patterns
+# process submitted values
