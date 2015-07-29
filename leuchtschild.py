dcfa2bce9f824f2e83db3b1ae092ae0cdbb498ad
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-07-29 21:59:42 +0200

    draft leuchtschild control

--- leuchtschild.py
+++ leuchtschild.py
@@ -3,18 +3,28 @@ The leuchtschild module allows control of the LED sign on the storefront.
 """
 
 from steuerwerk import app, ctrl_funcs
-from flask import render_template
+from flask import render_template, request
+
+class Pattern():
+	def __init__(self, selected=False):
+		self.selected = selected
 
 patterns = {
-	"eye cancer": None,
-	"red green blue": None,
-	"CSD": None,
-	"in Farbe und bunt": None
+	"eye cancer": Pattern(),
+	"red green blue": Pattern(),
+	"CSD": Pattern(),
+	"in Farbe und bunt": Pattern()
 }
+
+patterns["CSD"].selected = True
+
 
 ctrl_funcs["control Leuchtschild"] = "leuchtschild"
 @app.route('/leuchtschild/',methods=["GET","POST"])
 def show_sign():
+	if request.form:
+		pattern = request.form["pattern"]
+		color = request.form["color"]
 	return render_template("leuchtschild.html",patterns=patterns)
 
 ## TODO ##
