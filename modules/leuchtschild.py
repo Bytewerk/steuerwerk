70b2dae64246cc53367d0172d1d8b3e7576f8ee7
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-08-29 01:32:17 +0200

    [leuchtschild] de-dumb template, show currently selected pattern

--- modules/leuchtschild.py
+++ modules/leuchtschild.py
@@ -6,27 +6,31 @@ from steuerwerk import app, ctrl_funcs
 from flask import render_template, request
 
 class Pattern():
-	def __init__(self, selected=False):
-		self.selected = selected
+	def __init__(self):
+		pass
 
 patterns = {
 	"eye cancer": Pattern(),
 	"red green blue": Pattern(),
 	"CSD": Pattern(),
-	"in Farbe und bunt": Pattern()
+	"in Farbe und bunt": Pattern(),
+	"custom color": Pattern()
 }
 
-patterns["CSD"].selected = True
-custom_color = 0x0;
+active_pattern = "CSD"
+custom_color = 0x0
 
 ctrl_funcs["control Leuchtschild"] = "leuchtschild"
 @app.route('/leuchtschild/',methods=["GET","POST"])
 def show_sign():
+	global active_pattern
+	print(active_pattern)
 	if request.form:
-		pattern = request.form["pattern"]
+		active_pattern = request.form["pattern"]
+		print("new pattern: ",active_pattern)
 		global custom_color
 		custom_color = request.form["color"]
-	return render_template("leuchtschild.html",patterns=patterns,custom_color=custom_color)
+	return render_template("leuchtschild.html",patterns=patterns,custom_color=custom_color, active_pattern = active_pattern)
 
 ## TODO ##
 # Indicate currently active pattern in web interface
