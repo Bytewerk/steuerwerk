143e7171edce2fb5bcb9d16ae3d58487c0824bba
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-09-05 00:45:05 +0200

    fix indentation

--- modules/infrared.py
+++ modules/infrared.py
@@ -15,37 +15,37 @@ import sys
 CMDS_FILE = "ircodes.json"
 devices = None
 with open(CMDS_FILE) as cmds_file:
-    devices_raw = json.load(cmds_file, object_pairs_hook=collections.OrderedDict)
+	devices_raw = json.load(cmds_file, object_pairs_hook=collections.OrderedDict)
 
-    # flatten the multiple command dictionaries for every device into one
-    devices = {}
-    for key in devices_raw.keys():
-        devices[key] = {}
-        devices[key]["commands"] = {}
-        devices[key]["prot"] = devices_raw[key]["prot"]
-        for cmds in devices_raw[key]["commands"]:
-            devices[key]["commands"].update(cmds.items())
+	# flatten the multiple command dictionaries for every device into one
+	devices = {}
+	for key in devices_raw.keys():
+		devices[key] = {}
+		devices[key]["commands"] = {}
+		devices[key]["prot"] = devices_raw[key]["prot"]
+		for cmds in devices_raw[key]["commands"]:
+			devices[key]["commands"].update(cmds.items())
 
 ctrl_funcs["send ir commands"] = "ir"
 @app.route('/ir/', methods=['GET'])
 @app.route('/ir/<device>', methods=['GET','POST'])
 def show_ir(device=None):
-    if request.form and device:
-        name = request.form['cmd']
-        cmd = devices[device]["commands"][name]
-        prot = devices[device]["prot"]
-        if name == "MAKE IT SO":
-            make_it_so()
-        else:
-            send_cmds(prot,cmd)
-    return render_template("ir.html", devices = devices_raw)
+	if request.form and device:
+		name = request.form['cmd']
+		cmd = devices[device]["commands"][name]
+		prot = devices[device]["prot"]
+		if name == "MAKE IT SO":
+			make_it_so()
+		else:
+			send_cmds(prot,cmd)
+	return render_template("ir.html", devices = devices_raw)
 
 def make_it_so():
-    dev = devices["Amplifier"]
-    prot = dev["prot"]
-    cmds = dev["commands"]
-    send_cmds(prot, cmds["ON/OFF"], *[cmds["VOLUME UP"]]*15)
-    send_cmds(prot, cmds["DVD"])
+	dev = devices["Amplifier"]
+	prot = dev["prot"]
+	cmds = dev["commands"]
+	send_cmds(prot, cmds["ON/OFF"], *[cmds["VOLUME UP"]]*15)
+	send_cmds(prot, cmds["DVD"])
 
 
 COMMAND_TIMEOUT = 0.1 #timeout between actual sent IR commands
@@ -56,15 +56,15 @@ ECMD_HOST = "ir.bingo"
 ECMD_PORT = 2701
 
 def send_cmds(prot,*cmds):
-    """ Send the specified infrared commands. """
-    sock = None
-    for i in range(RECONNECTION_ATTEMPTS):
-        try:
-            sock = socket.create_connection((ECMD_HOST,ECMD_PORT),RECONNECTION_TIMEOUT)
-        except:
-            traceback.print_exc(file=sys.stderr)
-            time.sleep(RECONNECTION_TIMEOUT)
-    for cmd in cmds:
-        cmd_str = "irmp send {} {} 00\n".format(prot,cmd)
-        sock.send(bytes(cmd_str,"utf-8"))
-        time.sleep(COMMAND_TIMEOUT)
+	""" Send the specified infrared commands. """
+	sock = None
+	for i in range(RECONNECTION_ATTEMPTS):
+		try:
+			sock = socket.create_connection((ECMD_HOST,ECMD_PORT),RECONNECTION_TIMEOUT)
+		except:
+			traceback.print_exc(file=sys.stderr)
+			time.sleep(RECONNECTION_TIMEOUT)
+	for cmd in cmds:
+		cmd_str = "irmp send {} {} 00\n".format(prot,cmd)
+		sock.send(bytes(cmd_str,"utf-8"))
+		time.sleep(COMMAND_TIMEOUT)
--- steuerwerk.py
+++ steuerwerk.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+    abort, render_template, flash
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -9,11 +9,11 @@ ctrl_funcs = {}
 
 @app.route('/')
 def show_index():
-    """ Show an index of available steuerwerk functionality. """
-    return render_template("index.html", appname="steuerwerk", ctrl_funcs=ctrl_funcs)
+	""" Show an index of available steuerwerk functionality. """
+	return render_template("index.html", appname="steuerwerk", ctrl_funcs=ctrl_funcs)
 
 from modules.infrared import *
 from modules.leuchtschild import *
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
