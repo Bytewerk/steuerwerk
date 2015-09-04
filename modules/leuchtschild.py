f285aad8d74bc91db9bdba36ce4e9ab52d7f39cc
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-09-04 22:57:30 +0200

    use numeric pattern ids from leuchtschild firmware

--- modules/leuchtschild.py
+++ modules/leuchtschild.py
@@ -5,19 +5,14 @@ The leuchtschild module allows control of the LED sign on the storefront.
 from steuerwerk import app, ctrl_funcs
 from flask import render_template, request
 
-class Pattern():
-	def __init__(self):
-		pass
-
 patterns = {
-	"eye cancer": Pattern(),
-	"red green blue": Pattern(),
-	"CSD": Pattern(),
-	"in Farbe und bunt": Pattern(),
-	"custom color": Pattern()
+	0: "led_clear",
+	1: "fading",
+	2: "fading_kitsch",
+	-1: "custom color"
 }
 
-active_pattern = "CSD"
+active_pattern = 0
 custom_color = 0x0
 
 ctrl_funcs["control Leuchtschild"] = "leuchtschild"
@@ -26,13 +21,8 @@ def show_sign():
 	global active_pattern
 	print(active_pattern)
 	if request.form:
-		active_pattern = request.form["pattern"]
+		active_pattern = int(request.form["pattern"])
 		print("new pattern: ",active_pattern)
 		global custom_color
 		custom_color = request.form["color"]
 	return render_template("leuchtschild.html",patterns=patterns,custom_color=custom_color, active_pattern = active_pattern)
-
-## TODO ##
-# Indicate currently active pattern in web interface
-# Give previews/descriptions of patterns
-# process submitted values
